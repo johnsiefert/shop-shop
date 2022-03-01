47fc41587bc3f4c49052e2566a886b161a8b269d
johnsiefert <johnsief12@gmail.com>
2022-03-01 18:54:00 -0500

    lesson 1

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+    </div>
+  );
+};
